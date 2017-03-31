f9ebeed5e0171d4728589caa1dfa3f0d90fe1c32
Tomas Groth Christensen <tgc@dma.dk>
2017-03-31 15:15:08 +0200

    Added script to build javadocs and push to github pages.

---
+++ javadocs.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ "$1" = "docs" ]; then
+    mvn javadoc:javadoc
+    exit
+elif [ "$1" = "site" ]; then
+    echo "Checking for changes to the manual"
+    git status
+    if ! git diff-index --quiet HEAD --; then
+        echo "There are uncommitted changes"
+        exit 1
+    fi
+    rm -rf target/site
+    mvn javadoc:javadoc
+    
+    git checkout gh-pages
+    cp -r target/site/apidocs/ apidocs
+
+    git add apidocs/*
+    git commit -m "update javadocs"
+    git push
+    git checkout master
+    exit   
+elif [ -z "$1" ]; then 
+    echo Usage: $0 target
+    echo where target is:
+else
+    echo Unknown target: "$1"
+    echo Valid targets are:
+fi
+
+echo "  docs       Generates javadoc"
+echo "  site       Generates javadoc and push to GitHub Pages"
