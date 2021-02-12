7638606328244f0e5469176cca8cf80b71844b0e
Oliver Haagh <ohaagh@gmail.com>
2021-02-12 11:52:09 +0100

    There might be newline characters after the pem footer

--- src/main/java/net/maritimeconnectivity/pki/CertificateHandler.java
+++ src/main/java/net/maritimeconnectivity/pki/CertificateHandler.java
@@ -227,7 +227,7 @@ public class CertificateHandler {
     public static X509Certificate getCertFromNginxHeader(String certificateHeader) throws UnsupportedEncodingException {
         String certificateContent = URLDecoder.decode(certificateHeader, "UTF-8");
         // make sure that the + characters in the base64 encoded part have not been converted to spaces
-        if (certificateContent.startsWith(PKIConstants.CERT_HEADER) && certificateContent.endsWith(PKIConstants.CERT_FOOTER)) {
+        if (certificateContent.startsWith(PKIConstants.CERT_HEADER) && certificateContent.contains(PKIConstants.CERT_FOOTER)) {
             String middle = certificateContent.split(PKIConstants.CERT_HEADER)[1].split(PKIConstants.CERT_FOOTER)[0];
             if (middle.contains(" ")) {
                 middle = middle.replace(" ", "+");
